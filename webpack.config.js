2a4b103a9feb20e57cfe8f061e458f66f01ce8bb
Takao NAITO <xarsh@outlook.com>
2017-05-10 16:47:15 +0900

    Tidy.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/main.js',
@@ -30,13 +30,13 @@ module.exports = {
     hints: false
   },
   devtool: '#inline-source-map'
-}
+};
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+  module.exports.devtool = '#source-map';
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({'process.env': {NODE_ENV: '"production"'}}),
     new webpack.optimize.UglifyJsPlugin({sourceMap: true, compress: {warnings: false}}),
     new webpack.LoaderOptionsPlugin({minimize: true})
-  ])
+  ]);
 }
